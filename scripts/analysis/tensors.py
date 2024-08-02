0e79b431638ae07ec60cc22144bcbf6eda68198e
Sara Tolosa Alarcón <saratolosaalarcon@gmail.com>
2024-08-02 16:34:33 +0200

    tensor.py update

--- scripts/analysis/tensors.py
+++ scripts/analysis/tensors.py
@@ -6,17 +6,17 @@ from basic import amino_acids
 
 class Tensor:
     def __init__(self, tensor_path):
-
-        self.tensor_name=Path(tensor_path).stem
+        self.tensor_name = Path(tensor_path).stem
         self.tensor_path = tensor_path
         self.tensor_file = self.load_tensor()
-
+        
     def load_tensor(self):
         try:
-            self.file = torch.load(self.tensor_path)
-            if not isinstance(self.file, torch.Tensor):
+            tensor = torch.load(self.tensor_path)
+            if not isinstance(tensor, torch.Tensor):
                 raise TypeError("Loaded object is not a tensor")
-            return self.file
+            
+            return tensor
         
         except FileNotFoundError:
             raise FileNotFoundError(f"Tensor file '{self.tensor_path}' not found.")
@@ -24,51 +24,39 @@ class Tensor:
             raise Exception(f"Failed to load tensor: {str(e)}")
         
     def get_shape(self):
-        """ It returns the tensor shape."""
-        if not hasattr(self,"file"):
-            self.tensor_file()
-            
-        return self.file.shape
+        """ It returns the tensor shape."""     
+        return self.tensor_file.shape
     
     def remove_batch_size(self):
-        if not hasattr(self,"file"):
-            self.tensor_file()
+        """Remove the batch_size dimension of the tensor."""
+        dims = self.tensor_file.dim()
+        if self.tensor_file.shape[0] == 1 and dims > 3:
+            self.tensor_file = self.tensor_file.squeeze(dim=0)
 
-        dims = self.file.dim()
-        if self.file.shape[0] == 1 and dims > 3:
-            self.file = self.file.squeeze(dim=0)
-
-        return self.file
+        return self.tensor_file
     
     def normalize_tensor(self):
         """Normalize last dimension of a tensor."""
-
-        if not hasattr(self,"file"):
-            self.tensor_file()
-
-        normalized_tensor = torch.nn.functional.normalize(self.file, p=2, dim=-1)
-        self.file = normalized_tensor
+        normalized_tensor = torch.nn.functional.normalize(self.tensor_file, p=2, dim=-1)
+        self.tensor_file = normalized_tensor
 
         return self
     
 
     def tensor_reshaping(self, aa_list=amino_acids, mutated_sequence=str):
-        if not hasattr(self, "file"):
-            self.load_tensor()
-
-        seq_length = self.file.shape[-2]
+        """Reshape the tensor to the desired shape."""
+        seq_length = self.tensor_file.shape[-2]
         n_aa = len(aa_list)
         mut_seq = len(mutated_sequence)
-        emb_dim = self.file.shape[-1]
-        reshaped_tensor = self.file.reshape(mut_seq, n_aa, seq_length, emb_dim)
-        self.file = reshaped_tensor
+        emb_dim = self.tensor_file.shape[-1]
+        reshaped_tensor = self.tensor_file.reshape(mut_seq, n_aa, seq_length, emb_dim)
+        self.tensor_file = reshaped_tensor
 
         return self
 
     def preprocessing(self):
-        # Remove the batch_size dimension
+        """Preprocess the tensor by removing batch size and normalizing it."""
         self.remove_batch_size()
-        # Normalize the tensor
         self.normalize_tensor()
 
         return self 
