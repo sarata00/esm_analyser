fbd2b1c97fe99b1d26429b1b5eb07da6b8ad00e3
Sara Tolosa Alarcón <saratolosaalarcon@gmail.com>
2024-08-13 13:08:32 +0200

    command line interface

--- setup.py
+++ setup.py
@@ -3,9 +3,6 @@ import setuptools
 with open("README.md", "r") as f:
     readme = f.read()
 
-with open("requirements.txt", "r") as f:
-    install_requirements = f.read().splitlines()
-
 setuptools.setup(
     name="Embedding analyzer",
     version="1.0",
@@ -17,11 +14,11 @@ setuptools.setup(
     
     packages=setuptools.find_packages(exclude=["tests"]),
     keywords=["esm-2", "embedding", "correlation analysis", "dimensionality reduction"],
-    python_requires=">3.6",
-    install_requirements=install_requirements,
     entry_points={
         "console_scripts": [
-            "embedding_analyzer=scripts.analysis.__main__:main",
+            "embedding_analyzer = scripts.analysis.__main__:main",
+            "embedding_generator_esm = scripts.embedding_generators.embedding_generator_esm:main",
+            "embedding_generator_HF = scripts.embedding_generators.embedding_generator_hugging_face:main",
         ]
     }, 
 )
